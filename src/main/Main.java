94d5546af07a77e154b6839c62681c1fad29bd8d
Agarimo <Agarimo@192.168.1.35>
2014-01-14 17:45:58 +0100

    Commit Inicial

--- src/main/Main.java
+++ src/main/Main.java
@@ -22,7 +22,7 @@ public class Main {
     public static void main(String[] args) throws IOException, InterruptedException {
         driverAndInit();
 
-        args = new String[]{"-drop"};
+//        args = new String[]{"-drop"};
         if (args.length != 0) {
             inicio(args);
         }
