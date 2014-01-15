c32788314b1123a9d1e4c11c617b18a7d153faca
Agarimo <Agarimo@192.168.1.35>
2014-01-15 22:31:56 +0100

    Mejora de la Clase Mail

--- src/drop/Mail.java
+++ src/drop/Mail.java
@@ -22,9 +22,28 @@ public class Mail {
     private String msg;
 
     public Mail() {
+        this.usuario = "notify@electromegusta.es";
+        this.pass = "IkuinenK@@m.s84";
+        this.from = "notify@electromegusta.es";
+        this.to = "b2b@electromegusta.es";
+        this.subjet = "NUEVO PEDIDO!";
+        this.msg = "Existe un nuevo pedido en la tienda"
+                + "\nPara entrar en el panel pulse el link"
+                + "\n http://electromegusta.es/admin5570/";
     }
 
     public Mail(String from, String to, String subjet, String msg) {
+        this.usuario = "notify@electromegusta.es";
+        this.pass = "IkuinenK@@m.s84";
+        this.from = from;
+        this.to = to;
+        this.subjet = subjet;
+        this.msg = msg;
+    }
+    
+    public Mail(String usuario,String pass,String from, String to, String subjet, String msg) {
+        this.usuario = usuario;
+        this.pass = pass;
         this.from = from;
         this.to = to;
         this.subjet = subjet;
@@ -40,25 +59,16 @@ public class Mail {
             props.setProperty("mail.smtp.port", "25");
             props.setProperty("mail.smtp.auth", "true");
 
-
             // Preparamos la sesion
             Session session = Session.getDefaultInstance(props);
 
-            //Recoger los datos
-            String str_from = "notify@electromegusta.es";
-            String str_De = "notify@electromegusta.es";
-            String str_PwRemitente = "IkuinenK@@m.s84";
-            String str_Para = "b2b@electromegusta.es";
-            String str_Asunto = "NUEVO PEDIDO!";
-            String str_Mensaje = "Existe un nuevo pedido en la tienda"
-                    + "\nPara entrar en el panel pulse el link\n http://electromegusta.es/admin5570/";
             //Obtenemos los destinatarios
-            String destinos[] = str_Para.split(",");
+            String destinos[] = this.to.split(",");
 
             // Construimos el mensaje
             MimeMessage message = new MimeMessage(session);
 
-            message.setFrom(new InternetAddress(str_from));
+            message.setFrom(new InternetAddress(this.from));
 
             Address[] receptores = new Address[destinos.length];
             int j = 0;
@@ -69,19 +79,18 @@ public class Mail {
 
             //receptores.
             message.addRecipients(Message.RecipientType.TO, receptores);
-            message.setSubject(str_Asunto);
-            message.setText(str_Mensaje);
+            message.setSubject(this.subjet);
+            message.setText(this.msg);
 
             // Lo enviamos.
             Transport t = session.getTransport("smtp");
-            t.connect(str_De, str_PwRemitente);
+            t.connect(this.usuario, this.pass);
             t.sendMessage(message, message.getRecipients(Message.RecipientType.TO));
 
             // Cierre de la conexion.
             t.close();
             return true;
         } catch (Exception e) {
-            e.printStackTrace();
             return false;
         }
     }
